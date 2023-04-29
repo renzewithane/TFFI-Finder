a958c41aaa155ce2f4480db6d16f4381acf1fc19
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2023-04-29 14:04:08 +0800

    Uploaded Splash Screen

---
+++ app/src/main/java/com/example/tffifinder/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.tffifinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar pb;
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prog();
+    }
+
+    public void prog() {
+        pb = findViewById(R.id.pb);
+
+        final Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                pb.setProgress(counter);
+
+                if(counter == 100) {
+                    t.cancel();
+                    Intent intent = new Intent(MainActivity.this, Home.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        t.schedule(tt, 0, 30);
+    }
+}
