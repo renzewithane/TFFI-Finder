2839abdfb69e134fdd09f423f15680ecb77add9e
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2023-05-06 10:21:07 +0800

    Edited Credits

--- app/src/main/java/com/example/tffifinder/Credits.java
+++ app/src/main/java/com/example/tffifinder/Credits.java
@@ -1,8 +1,10 @@
 package com.example.tffifinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Credits extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class Credits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Set the custom back arrow as the navigation icon
+        myToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+
+        // Set a click listener on the navigation icon
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    public void setSupportActionBar(Toolbar myToolbar) {
     }
 }
